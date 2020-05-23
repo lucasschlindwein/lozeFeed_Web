3435743b7e30ce6ad89f4e79727fdd6edb22e55a
Schilin <schilin.00@gmail.com>
2020-05-23 00:39:20 -0300

    create feed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+
+import './Feed.css';
+
+import like from '../assets/like.svg';
 
 class Feed extends Component{
+  state = {
+    feed: [],
+  };
+  async componentDidMount(){
+    const response = await api.get('posts');
+
+    this.setState({feed: response.data});
+  }
+
   render(){
     return (
-      <h1>Feed</h1>
+      <section id='post-list'>
+        {this.state.feed.map(post => (
+          <article>
+          <header>
+            <div className='user-info'>
+              <span>{post.author}</span>
+              <span className='place'>{post.place}</span>
+            </div>
+          </header>
+
+          <img src={`http>//localhost:3001/files/${post.image}`} alt='' />
+
+          <footer>
+            <div className='actions'>
+              <img src={like} alt='' />
+            </div>
+
+            <strong>{post.likes} curtidas</strong>
+
+            <p>
+              {post.description}
+              <span>{post.hashtags}</span>
+            </p>
+          </footer>
+        </article>
+        ))}
+      </section>
     );
   }
 }
