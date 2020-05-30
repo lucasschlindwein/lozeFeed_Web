bdd8fa745505d28443b104b3e65f173afc103e00
Schilin <schilin.00@gmail.com>
2020-05-30 17:08:40 -0300

    .env add

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -3,7 +3,6 @@ import api from '../services/api';
 import io from 'socket.io-client';
 
 import './Feed.css';
-
 import like from '../assets/like.svg';
 
 class Feed extends Component{
@@ -19,7 +18,8 @@ class Feed extends Component{
   }
 
   registerToSocket = () => {
-    const socket = io('http://localhost:3001');
+    // const socket = io('http://localhost:3001');
+    const socket = io(process.env.REACT_APP_API_URL);
 
     socket.on('post', newPost => {
       this.setState({ feed: [newPost, ...this.state.feed] });
